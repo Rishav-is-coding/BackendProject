dc5c2fd42126fabea9d6e105d245aa16fcf80eaa
Rishav-is-coding <rishavraj02012003@gmail.com>
2025-07-04 12:24:30 +0530

    fix health check response: update health check controller to use ApiResponse for consistent response formatting

--- src/controllers/healthCheck.controller.js
+++ src/controllers/healthCheck.controller.js
@@ -2,7 +2,7 @@ import {ApiResponse} from '../utils/ApiResponse.js'
 import {asyncHandler} from '../utils/asyncHandler.js'
 
 const healthCheck = asyncHandler(async (req, res) => {
-    return res.status(200).json(200, null, "health Check OK")
+    return res.status(200).json(new ApiResponse(200, null, "health Check OK"))
 })
 
 export {healthCheck}
