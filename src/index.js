8a70c102b05f7528bdf6eb5f350897e23b252621
Rishav-is-coding <rishavraj02012003@gmail.com>
2025-07-21 10:46:22 +0530

    fix: correct server port logging to ensure fallback to default value

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ connetDB()
         })
 
         app.listen(process.env.PORT || 8000, () => {
-            console.log("Server is running at PORT : ", process.env.PORT)
+            console.log("Server is running at PORT : ", process.env.PORT || 8000)
         })
     })
     .catch((error) => {
